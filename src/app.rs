229e9fa201eb959b03c818527c907343b90934cb
Daniel Raniz Raneland <raniz@factor10.com>
2024-02-28 23:03:17 +0100

    Update dependencies, add random button

--- src/app.rs
+++ src/app.rs
@@ -2,21 +2,22 @@ use std::rc::Rc;
 use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
-use rand::random;
+use leptos_icons::Icon;
+use icondata as i;
 use rand::seq::IndexedRandom;
 use crate::knot::{Knot, Knots};
 
 const KNOTS: &str = include_str!("knots.toml");
 
 #[component]
-pub fn App(cx: Scope) -> impl IntoView {
+pub fn App() -> impl IntoView {
     // Provides context that manages stylesheets, titles, meta tags, etc.
-    provide_meta_context(cx);
+    provide_meta_context();
 
     let knots: Knots = toml::from_str(KNOTS).unwrap();
     let knots2 = knots.clone();
 
-    view! { cx,
+    view! {
         // injects a stylesheet into the document <head>
         // id=leptos means cargo-leptos will hot-reload this stylesheet
         <Stylesheet id="leptos" href="/pkg/neckties.css"/>
@@ -30,11 +31,11 @@ pub fn App(cx: Scope) -> impl IntoView {
                 <Routes>
                     <Route
                         path=""
-                        view=move |cx| view!{ cx, <KnotList knots={knots.clone()} /> }
+                        view=move || view!{ <KnotList knots={knots.clone()} /> }
                     />
                     <Route
                         path="/:slug"
-                        view=move |cx| view!{ cx, <Knot knots={knots2.clone()} /> }
+                        view=move || view!{ <Knot knots={knots2.clone()} /> }
                     />
                 </Routes>
             </main>
@@ -44,24 +45,27 @@ pub fn App(cx: Scope) -> impl IntoView {
 
 /// A list of knots
 #[component]
-fn KnotList(cx: Scope, knots: Knots) -> impl IntoView {
+fn KnotList(knots: Knots) -> impl IntoView {
     let mut rng = rand::thread_rng();
     let random = knots.knots.choose(&mut rng).unwrap();
 
-    view! { cx,
+    view! {
         <h1>"Neckties!"</h1>
         <ul>
             <li>
-                <a href={format!("/{}", &random.slug())}>Random</a>
+                <a href={format!("/{}", &random.slug())}><Icon icon=i::BiDice3Regular /></a>
             </li>
             {
                 knots.knots.iter()
-                    .map(|knot| view!{cx,
+                    .map(|knot| view!{
                         <li>
-                            <a href={format!("/{}", &knot.slug())}>{&knot.name}</a>
+                            <a href={format!("/{}", &knot.slug())}>
+                                <img src={format!("http://img.youtube.com/vi/{}/mqdefault.jpg", &knot.youtube)} />
+                                {&knot.name}
+                            </a>
                         </li>
                     })
-                    .collect_view(cx)
+                    .collect_view()
             }
         </ul>
     }
@@ -69,8 +73,8 @@ fn KnotList(cx: Scope, knots: Knots) -> impl IntoView {
 
 /// 404 - Not Found
 #[component]
-fn NotFound(cx: Scope) -> impl IntoView {
-    view! { cx,
+fn NotFound() -> impl IntoView {
+    view! {
         <h1>"No such necktie for you"</h1>
     }
 }
@@ -82,25 +86,25 @@ struct KnotParams {
 
 /// A knot
 #[component]
-fn Knot(cx: Scope, knots: Knots) -> impl IntoView {
-    let slug = use_params::<KnotParams>(cx).with(|params| params.as_ref()
+fn Knot(knots: Knots) -> impl IntoView {
+    let slug = use_params::<KnotParams>().with(|params| params.as_ref()
         .map(|params| params.slug.clone())
         .unwrap_or_default()
     );
     let knot = knots.knots.into_iter().find(|knot| knot.slug() == slug);
 
-    view! { cx,
+    view! {
         {
             move || if let Some(knot) = &knot {
-                view! { cx,
+                view! {
                     <h1>{knot.name.clone()}</h1>
                      <iframe width="1080" height="720" src={format!("https://www.youtube.com/embed/{}", knot.youtube.clone())}>
 
                     </iframe>
 
-                }.into_view(cx)
+                }.into_view()
             } else {
-                view! { cx, <NotFound /> }.into_view(cx)
+                view! { <NotFound /> }.into_view()
             }
         }
     }
